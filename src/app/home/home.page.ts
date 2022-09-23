0da19b3a0e798046e8789f7104189759efb6b245
Jean Souza <jsouza20603@gmail.com>
2022-09-23 20:26:18 -0300

    namoro

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  nome1: string;
+  nome2: string;
+  porcentagem: number;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  calcular() {
+    let url = 'http://lucasreno.kinghost.net/love-calculator/';
+    url = url + this.nome1 + '/' + this.nome2;
+
+    this.http.get(url).subscribe(resposta => this.porcentagem = +resposta);
+  }
 
 }
